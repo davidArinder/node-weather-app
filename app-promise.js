a594d111ab18a779732eeca37952182a8b436e07
David Arinder <david.arinder@gmail.com>
2019-04-09 12:17:50 -0700

    cleaning up code

--- app-promise.js
+++ app-promise.js
@@ -14,20 +14,20 @@ const argv = yargs
 .alias('help', 'h')
 .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-var geocodeUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=Dx0jhtAuSI5bVyFAX80O1M0KSF2fgOXY&location=${encodedAddress}`;
+const encodedAddress = encodeURIComponent(argv.address);
+const geocodeUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=Dx0jhtAuSI5bVyFAX80O1M0KSF2fgOXY&location=${encodedAddress}`;
 
 axios.get(geocodeUrl).then((response) => {
   if (response.info.statuscode === 400) {
     throw new Error('Unable to find that address.');
   }
 
-  var lat = ;
-  var lng;
-  var weatherUrl = `https://api.darksky.net/forecast/90e843684bc6864964be658d29c7a359/${lat},${lng}`
+  const lat = ;
+  const lng;
+  const weatherUrl = `https://api.darksky.net/forecast/90e843684bc6864964be658d29c7a359/${lat},${lng}`
   console.log(response.data);
 }).catch((e) => {
-  if (e.code = 'ENOTFOUND') {
+  if (e.code === 'ENOTFOUND') {
     console.log('Unable to connect to API servers.');
   } else {
     console.log(e.message);
